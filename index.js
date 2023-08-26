8e19c4a35f672593248e7b5724090abd2b66cf0a
Hamidul Islam <100404131+HamidTheDev@users.noreply.github.com>
2023-08-26 23:01:31 +0600

    server updated with best way

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const { Server } = require('socket.io')
-const schedule = require('node-schedule')
+import express from 'express'
+import cors from 'cors'
+import { Server as socketIoServer } from 'socket.io'
 
-const io = new Server(server, {
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// storing server to variable
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+// socket server attach with cors must
+const socket = new socketIoServer(server, {
   cors: {
     origin: '*',
   },
 })
 
-const PORT = process.env.PORT || 5000
-
+// emitting message to client
 const sendMessage = (message) => {
-  io.emit('message', message)
+  socket.emit('message', message)
 }
 
-// Schedule a job to run every 5 seconds
-schedule.scheduleJob('*/5 * * * * *', () => {
-  sendMessage('Hello from the server!')
-  console.log('first')
+app.get('/', (req, res) => {
+  sendMessage('Hello World!')
+  res.send('Hello World!')
 })
 
-io.listen(PORT, () =>
-  console.log(`Socket io server is running on port ${PORT}`)
-)
+
+// Schedule a job to run every 5 seconds
+// schedule.scheduleJob('*/5 * * * * *', () => {
+//   sendMessage('Hello from the server!')
+//   console.log('first')
+// })
